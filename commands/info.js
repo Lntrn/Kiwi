bfd74a15f7bb1c02265e1c93d4a0d5adff9291c3
Adam Elaoud <adam.elaoud@gmail.com>
2020-09-14 00:55:53 +0100

    added new functions for generating pages for info command

--- commands/info.js
+++ commands/info.js
@@ -13,21 +13,55 @@ module.exports = {
         // allow usage only if user is the owner
         if (msg.author.id === Data.ownerId) {
             let servers = bot.guilds.cache.array().sort();
-            let serverPrintout = "";
+            let pageCount = (servers.length / 20) + 1;
             let memberCount = 0;
 
             servers.forEach(
                 function(server) {
-                    joinDate = server.joinedAt;
-                    date = new Date();
-
-                    serverPrintout += `\n:white_small_square: **${server.name}** [${joinDate.toDateString()}]`;
                     memberCount += server.memberCount;
-
-                    if (joinDate.toDateString() === date.toDateString())
-                        serverPrintout += "🚩";
                 }
             );
+
+            msg.channel.send(module.exports.pageOne()).then(
+                function(sentMsg) {
+                    let page = 1;
+    
+                    // generate reactions
+                    sentMsg.react("⬅️");
+                    sentMsg.react("➡️");
+    
+                    // reaction filters
+                    const leftFilter = (reaction, user) => reaction.emoji.name === "⬅️" && user.id === msg.author.id;
+                    const rightFilter = (reaction, user) => reaction.emoji.name === "➡️" && user.id === msg.author.id;
+    
+                    // collectors (parse for 60 seconds)
+                    const leftCollector = sentMsg.createReactionCollector(leftFilter, {time: 60000});
+                    const rightCollector = sentMsg.createReactionCollector(rightFilter, {time: 60000});
+    
+                    leftCollector.on("collect", 
+                        function() {
+                            sentMsg.reactions.cache.get("⬅️").users.remove(msg.author);
+                            module.exports.resetTimer(leftCollector, rightCollector);
+    
+                            if (page !== 1) { 
+                                page--;
+                                sentMsg.edit(module.exports.generatePage(page));
+                            } 
+                        }
+                    );
+                    rightCollector.on("collect", 
+                        function() {
+                            sentMsg.reactions.cache.get("➡️").users.remove(msg.author);
+                            module.exports.resetTimer(leftCollector, rightCollector);
+    
+                            if (page !== pageCount) {
+                                page++;      
+                                sentMsg.edit(module.exports.generatePage(page));                                               
+                            } 
+                        }
+                    );
+                }
+            ).catch(err => console.log("Error adding reactions!" + err));
             
             const embed = new Discord.MessageEmbed()
                 .setColor("#D5AB88")
@@ -49,6 +83,33 @@ module.exports = {
             .setFooter(Data.footer.text, Data.footer.image);
 
             msg.channel.send(embed);
+        }
+    },
+    resetTimer(left, right) {
+        left.resetTimer({time: 60000});
+        right.resetTimer({time: 60000});
+    },
+    generatePage(servers, page) {
+        let start = page * 20;
+        
+        if (start <= servers.length) {
+            let printout = "";
+            let date = new Date();
+
+            let i;
+            for (i = start; (i < start + 20 && i < servers.length); i++) {
+                joinDate = servers[i].joinedAt;
+
+                printout += `\n:white_small_square: **${server.name}** [${joinDate.toDateString()}]`;
+
+                if (joinDate.toDateString() === date.toDateString())
+                    printout += "🚩";
+            }
+
+            return printout;
+
+        } else {
+            return "empty page";
         }
     }
 }
