95b2cba3480b3896cdb8bc5eea66bb9555c0b58a
Adam Elaoud <adam.elaoud@gmail.com>
2020-11-17 14:42:35 +0000

    updated bug command with new utility files

--- commands/bug.js
+++ commands/bug.js
@@ -1,9 +1,15 @@
 // require discord.js module
 const Discord = require("discord.js");
 // require data.js module
-const Data = require("../utilities/data.js");
+const Emojis = require("../utilities/emojis.js");
+// require data.js module
+const Format = require("../utilities/format.js");
+// require data.js module
+const Channels = require("../utilities/channels.js");
 // require mongo.js module
 const Mongo = require("../utilities/mongo.js");
+// require data.js module
+const Roles = require("../utilities/roles.js");
 // require error logger module
 const ErrorLog = require("../utilities/error.js");
 
@@ -14,31 +20,31 @@ module.exports = {
         let date = new Date();
 
         // react to command
-        msg.react(bot.emojis.cache.get(Data.emojiIds.kiwi));
+        msg.react(bot.emojis.cache.get(Emojis.kiwi.id));
 
         const report = new Discord.MessageEmbed()
             .setColor("#8899A6")
-            .setTitle(`:wrench:${Data.space(1)} **━━━━━━━━ BUG REPORT ━━━━━━━━** ${Data.space(1)}:wrench:`)
+            .setTitle(`:wrench:${Format.space(1)} **━━━━━━━━ BUG REPORT ━━━━━━━━** ${Format.space(1)}:wrench:`)
             .setDescription(`**Sent By:** ${msg.author}`
                             + `\n**Date:** ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`
                             + `\n**Time:** ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
                             + `\n**Server:** "${msg.guild.name}" (ID: ${msg.guild.id})`
                             + `\n\n**Report:** ${args.join(" ")}`);
 
-        bot.channels.cache.get(Data.bugReportId).send(report).catch(err => ErrorLog.log(bot, msg, msg.guild.id, "bug [sending report]", err));
-        bot.channels.cache.get(Data.bugReportId).send(Data.devReportRole).catch(err => ErrorLog.log(bot, msg, msg.guild.id, "bug [notifying dev]", err));
+        bot.channels.cache.get(Channels.bugReport.id).send(report).catch(err => ErrorLog.log(bot, msg, msg.guild.id, "bug [sending report]", err));
+        bot.channels.cache.get(Channels.bugReport.id).send(Roles.dev.pub).catch(err => ErrorLog.log(bot, msg, msg.guild.id, "bug [notifying dev]", err));
 
         const response = new Discord.MessageEmbed()
             .setColor("#8899A6")
-            .setTitle(`:wrench:${Data.space(1)} **━━━━━━━━ BUG REPORT ━━━━━━━━** ${Data.space(1)}:wrench:`)
-            .setDescription(`*Thank you for contributing to improving Kiwi!* ${Data.space(1)}${Data.emojis.kiwi}`
+            .setTitle(`:wrench:${Format.space(1)} **━━━━━━━━ BUG REPORT ━━━━━━━━** ${Format.space(1)}:wrench:`)
+            .setDescription(`*Thank you for contributing to improving Kiwi!* ${Format.space(1)}${Emojis.kiwi.pub}`
                             + `\n\n**Bug Report:**`
                             + `\n${args.join(" ")}`
-                            + `\n\n\n:white_check_mark: **Sucessfully Submitted to [Support Server](${Data.supportLink})!**`)
+                            + `\n\n\n:white_check_mark: **Sucessfully Submitted to [Support Server](${Format.supportLink})!**`)
             .addField("\u200b", "\u200b")
-            .addField("Like what you see?", `[**${Data.bot.text}**](${Data.bot.invite}) ${Data.emojis.kiwi}`
-                                        + `\n[**${Data.server.text}**](${Data.server.link}) ${Data.emojis.spiralscholars}`)
-            .setFooter(Data.footer.text, Data.footer.image);
+            .addField("Like what you see?", `[**${Format.bot.text}**](${Format.bot.invite}) ${Emojis.kiwi.pub}`
+                                        + `\n[**${Format.server.text}**](${Format.server.link}) ${Emojis.spiralscholars.pub}`)
+            .setFooter(Format.footer.text, Format.footer.image);
 
         msg.channel.send(response).catch(err => ErrorLog.log(bot, msg, msg.guild.id, "bug [submission reply]", err));
 
